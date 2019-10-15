ba3c5a827667cb7833a05ca31c0fa8af1e4a19d6
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-15 20:57:47 +0300

    Update search.js

--- JS Core/Advanced/05. jQuery/Lab/solutions/02. Search in list/search.js
+++ JS Core/Advanced/05. jQuery/Lab/solutions/02. Search in list/search.js
@@ -1,3 +1,10 @@
 function search() {
-   // TODO:
+   let input = $('#searchText').val()
+
+   let matches = $('ul#towns li')
+      .css('font-weight', 'normal')
+      .filter((index, li) => li.textContent.includes(input))
+      .css('font-weight', 'bold')
+
+   $('#result').text(`${matches.length} matches found.`)
 }
