fcdcc2373410a52a2b530e159aea2d8d572f9eae
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-15 21:08:17 +0300

    Update scripts.js

--- JS Core/Advanced/06. DOM manipulations/Exercise/solutions/05. Notification/scripts.js
+++ JS Core/Advanced/06. DOM manipulations/Exercise/solutions/05. Notification/scripts.js
@@ -1,11 +1,9 @@
 function notify(message) {
     $('#notification')
-        .css('display', 'block')  // block?
+        .css('display', 'block')  
         .text(message)
 
     $('button').click(setTimeout(() => {
         $('#notification').css('display', 'none')
     }, 2000))
 }
-
-// when the button is clicked twice or more the browser throws an error
