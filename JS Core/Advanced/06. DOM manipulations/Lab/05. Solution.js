acff679222c27fd89d0d25d13a5c3ffe05d9783b
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-15 18:20:25 +0300

    Add files via upload

---
+++ JS Core/Advanced/06. DOM manipulations/Lab/05. Solution.js
@@ -0,0 +1,10 @@
+// is jQuery allowed??
+function attachGradientEvents() {
+    document.getElementById('gradient').addEventListener('mousemove', onGradientClick)
+    function onGradientClick(e) {
+        let width = 300
+        let position = e.offsetX
+        let gradientPercentage = Math.floor((position ) / width * 100) + '%'
+        document.getElementById('result').textContent = gradientPercentage
+    }
+}
