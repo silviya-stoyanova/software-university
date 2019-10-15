26d76a186a23c59dfe72cbdf73aad79e150f0a0e
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-15 21:42:01 +0300

    Add files via upload

---
+++ JS Core/Apps/02. AJAX and jQuery AJAX/Exercise/solutions/03. Messenger/solution.js
@@ -0,0 +1,35 @@
+function attachEvents() {
+    $('#submit').on('click', submitMessage)
+    $('#refresh').on('click', refreshMessages)
+}
+
+function submitMessage() {
+    let author = $('#author').val()
+    let content = $('#content').val()
+    let timestamp = Date.now()
+
+    let newMessage = JSON.stringify({ author, content, timestamp })
+    $.ajax({
+        method: 'POST',
+        url: `https://messagesz.firebaseio.com/.json`,
+        data: newMessage
+    })
+        .then(id => refreshMessages)       //////
+}
+
+function refreshMessages() {
+    let response = $.get(`https://messagesz.firebaseio.com/.json`)
+    response.then((messages) => loadMessages(messages))
+}
+
+function loadMessages(messages) {
+    $('#messages').empty()
+    console.log(messages);
+    let allMessages = $('#messages').text()
+
+    for (let id in messages) {
+        // to sort them by time??
+        allMessages += `${messages[id].author}: ${messages[id].content}\n`
+    }
+    $('#messages').text(allMessages)
+}
