dba72ee317df0ae038b53e607853412a6cbcbada
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-15 21:00:23 +0300

    Update incrementCounter.js

--- JS Core/Advanced/05. jQuery/Exercises/solutions/01. Increment counter/incrementCounter.js
+++ JS Core/Advanced/05. jQuery/Exercises/solutions/01. Increment counter/incrementCounter.js
@@ -1,8 +1,24 @@
 function increment(selector) {
+    let textArea = $('<textarea class="counter" value="0" disabled>0</textarea>')
+    let incrementBtn = $('<button class="btn" id="incrementBtn">Increment</button>')
+    let addBtn = $('<button class="btn" id="addBtn">Add</button>')
+    let ul = $('<ul class="results"></ul>')
 
+    incrementBtn.click(increaseNum)
+    addBtn.click(addNum)
 
+    function increaseNum() {
+        textArea.val(Number(textArea.val()) + 1)
+    }
 
+    function addNum() {
+        let li = $(`<li>${textArea.val()}</li>`)
+        li.appendTo(ul)
+    }
 
-
-
+    $(selector)
+        .append(textArea)
+        .append(incrementBtn)
+        .append(addBtn)
+        .append(ul)
 }
