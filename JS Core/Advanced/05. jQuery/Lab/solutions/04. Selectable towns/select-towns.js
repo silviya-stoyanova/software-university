3098be45c1905e48f099703926fbb2237b8a6897
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-15 20:58:21 +0300

    Update select-towns.js

--- JS Core/Advanced/05. jQuery/Lab/solutions/04. Selectable towns/select-towns.js
+++ JS Core/Advanced/05. jQuery/Lab/solutions/04. Selectable towns/select-towns.js
@@ -1,3 +1,24 @@
 function attachEvents() {
-	// TODO:
+	$('ul#items li').on('click', clickTown)
+	$('#showTownsButton').click(showTowns)
+
+	function clickTown(event) {
+		let town = $(this)
+		if (typeof town.attr('data-selected') === 'undefined') {
+			town.attr('data-selected', true)
+				.css('background', '#DDD')
+		} else {
+			town.removeAttr('data-selected')
+				.css('background', 'none')
+		}
+	}
+
+	function showTowns() {
+		console.log($('#items li[data-selected="true"]'))
+		let towns = $('#items li[data-selected="true"]')
+		$('#selectedTowns')
+			.append(towns.toArray()
+				.map(town => town.textContent)
+				.join(', '))
+	}
 }
