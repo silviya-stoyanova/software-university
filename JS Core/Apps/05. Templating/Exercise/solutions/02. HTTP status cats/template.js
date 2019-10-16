4f63d47fa6634c3b998295de28fab8787f775c39
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-16 13:49:59 +0300

    Add files via upload

---
+++ JS Core/Apps/05. Templating/Exercise/solutions/02. HTTP status cats/template.js
@@ -0,0 +1,25 @@
+$(() => {
+    renderCatTemplate();
+
+    function renderCatTemplate() {
+        // TO DO: Render cat template and attach events
+        let html = $.get('./catTemplate.hbs')
+        html.then(templateHTML => {
+            let template = Handlebars.compile(templateHTML)
+            let context = { cats }
+            let renderedHTML = template(context)
+            $('#allCats').append(renderedHTML)
+            $('.btn').on('click', showStatusCode)
+        })
+    }
+
+    function showStatusCode(e) {
+        let extraInfoDiv = $(e.target).siblings()
+        extraInfoDiv.toggle()
+        let button = extraInfoDiv.prev()
+
+        button.text() === 'Show status code'
+            ? button.text('Hide status code')
+            : button.text('Show status code')
+    }
+})
