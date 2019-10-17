e0e212e4a64694044ead4c283a8cf0e2fac43cab
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-17 20:25:48 +0300

    Add files via upload

---
+++ JS Core/Fundamentals/02. DOM/Lab/solutions/04. Register notification/solution.js
@@ -0,0 +1,33 @@
+function register() {
+  //TO DO
+  let usernameElement = document.getElementById('username')
+  let emailElement = document.getElementById('email')
+  let passwordElement = document.getElementById('password')
+
+  let username = usernameElement.value
+  let email = emailElement.value
+  let password = passwordElement.value
+
+  if (username.length > 0 && email.match(/(.+)@(.+).(com|bg)/gm) && password.length > 0) {
+    username = document.createTextNode(`Username: ${username}`)
+    email = document.createTextNode(`Email: ${email}`)
+    password = document.createTextNode(`Password: ${'*'.repeat(password.length)}`)
+
+    let createdHeading = document.createElement('h1')
+    createdHeading.innerText = 'Successful registration!'
+    let brTag = document.createElement('br')
+    let brTag2 = document.createElement('br')
+
+    let resultElement = document.getElementById('result')
+    resultElement.appendChild(createdHeading)
+    resultElement.appendChild(username)
+    resultElement.appendChild(brTag)
+    resultElement.appendChild(email)
+    resultElement.appendChild(brTag2)
+    resultElement.appendChild(password)
+
+    usernameElement.value = ''
+    emailElement.value = ''
+    passwordElement.value = ''
+  }
+}
