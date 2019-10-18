0671fe1d2fbfe4824419e5fee429540f3cbfaa37
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-18 12:13:32 +0300

    Add files via upload

---
+++ JS Core/Fundamentals/03. Functions and logic flow/Exercise/solutions/10. Factors/solution.js
@@ -0,0 +1,19 @@
+﻿function solve() {
+   let number = Number(document.getElementById('num').value)
+   calculateFactors()
+
+   function calculateFactors() {
+      let factors = []
+
+      for (let i = 1; i <= number; i++) {    // а отрицателните числа?
+         let result = number / i
+
+         if (result === Math.round(result)) { // if num is integer
+            factors.push(i)
+         }
+      }
+
+      factors.sort((a, b) => a - b)
+      document.getElementById('result').textContent = factors.join(' ')
+   }
+}
