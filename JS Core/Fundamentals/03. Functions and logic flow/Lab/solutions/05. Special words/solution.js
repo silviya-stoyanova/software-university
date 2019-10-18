2ba1e9a76efa011d610886ae66e8a1ee7f904fbd
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-18 20:02:19 +0300

    Update solution.js

--- JS Core/Fundamentals/03. Functions and logic flow/Lab/solutions/05. Special words/solution.js
+++ JS Core/Fundamentals/03. Functions and logic flow/Lab/solutions/05. Special words/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  // TODO
+  let startingNum = Number(document.getElementById('firstNumber').value)
+  let endingNum = Number(document.getElementById('secondNumber').value)
+  let firstStr = document.getElementById('firstString').value
+  let secondStr = document.getElementById('secondString').value
+  let thirdStr = document.getElementById('thirdString').value
+
+  function printNums() {
+    for (let i = startingNum; i <= endingNum; i++) {
+      let paragraphElement = document.createElement('p')
+      let output = `${i}`
+
+      if (i % 3 === 0 && i % 5 === 0) {
+        output += ` ${firstStr}-${secondStr}-${thirdStr}`
+
+      } else if (i % 3 === 0) {
+        output += ` ${secondStr}`
+
+      } else if (i % 5 === 0) {
+        output += ` ${thirdStr}`
+      }
+
+      paragraphElement.textContent = output
+      document.getElementById('result').appendChild(paragraphElement)
+    }
+  }
+
+  printNums()
 }
