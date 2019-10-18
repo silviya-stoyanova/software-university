eecea2a06adf536fa38b9ad6e3b95f6a9e35a453
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-18 11:47:45 +0300

    Add files via upload

---
+++ JS Core/Fundamentals/03. Functions and logic flow/Lab/solutions/03. Count occurrences of a given character/solution.js
@@ -0,0 +1,17 @@
+function solve() {
+  // TO DO
+  let string = [...document.getElementById('string').value]
+  let char = document.getElementById('character').value
+  let couter = 0
+  findOccurences()
+
+  function findOccurences() {
+    string.forEach(symbol => {
+      if (symbol === char) {
+        couter++
+      }
+    })
+
+    document.getElementById('result').textContent = `Count of ${char} is ${couter % 2 === 0 ? 'even' : 'odd'}.`
+  }
+}
