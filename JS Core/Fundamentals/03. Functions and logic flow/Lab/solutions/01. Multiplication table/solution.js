c76d936539f46a547d70aa55d26e8a0f2c242dc5
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-18 19:55:13 +0300

    Update solution.js

--- JS Core/Fundamentals/03. Functions and logic flow/Lab/solutions/01. Multiplication table/solution.js
+++ JS Core/Fundamentals/03. Functions and logic flow/Lab/solutions/01. Multiplication table/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  // TODO
+  let firstNum = Number(document.getElementById('num1').value)
+  let secondNum = Number(document.getElementById('num2').value)
+  let resultElement = document.getElementById('result')
+  printTable(firstNum, secondNum)
+
+  function printTable(firstNum, secondNum) {
+    if (firstNum > secondNum) {
+      return resultElement.innerHTML = 'Try with other numbers.'
+    }
+
+    for (let i = firstNum; i <= secondNum; i++) {
+      let ouptutParagraph = document.createElement('p')
+      ouptutParagraph.innerHTML = `${i} * ${secondNum} = ${i * secondNum}`
+      resultElement.appendChild(ouptutParagraph)
+    }
+  }
 }
