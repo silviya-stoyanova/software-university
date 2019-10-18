5948142ac78a79014cb64a297ae976dd37e5d774
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-18 19:55:48 +0300

    Update solution.js

--- JS Core/Fundamentals/03. Functions and logic flow/Lab/solutions/02. Temperature converter/solution.js
+++ JS Core/Fundamentals/03. Functions and logic flow/Lab/solutions/02. Temperature converter/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  // TODO
+  let inputDegrees = Number(document.getElementById('num1').value)
+  let inputUnit = document.getElementById('type').value.toLowerCase()
+  let resultElement = document.getElementById('result')
+  convertTemperature()
+
+  function convertTemperature() {
+    let result = Number
+
+    if (inputUnit === 'fahrenheit') {
+      result = Math.round((inputDegrees - 32) / 1.8)
+
+    } else if (inputUnit === 'celsius') {
+      result = Math.round(inputDegrees * 1.8 + 32)
+
+    } else {
+      result = 'Error!'
+    }
+
+    resultElement.textContent = result
+  }
 }
