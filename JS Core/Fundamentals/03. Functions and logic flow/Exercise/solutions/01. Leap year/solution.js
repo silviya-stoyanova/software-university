a9cd8b0abbe821660c4b8c749f60cd9ba181cda0
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-18 20:19:46 +0300

    Add files via upload

---
+++ JS Core/Fundamentals/03. Functions and logic flow/Exercise/solutions/01. Leap year/solution.js
@@ -0,0 +1,27 @@
+function leapYear() {
+    // TO DO:
+    document.getElementsByTagName('button')[0]
+        .addEventListener('click', checkYearType)
+
+    function checkYearType() {
+        let yearElement = document.querySelector('#exercise input')
+        let year = Number(yearElement.value)
+        let yearType = ''
+
+        if (year % 4 === 0) {
+            yearType = 'Leap Year'
+            if (year % 100 === 0) {
+                yearType = 'Not Leap Year'
+                if (year % 400 === 0) {
+                    yearType = 'Leap Year'
+                }
+            }
+        } else {
+            yearType = 'Not Leap Year'
+        }
+
+        yearElement.value = ''
+        document.querySelector('#year h2').textContent = yearType
+        document.querySelector('#year div').textContent = year
+    }
+}
