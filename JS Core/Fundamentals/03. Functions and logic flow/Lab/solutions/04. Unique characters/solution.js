a21af2cdd1948f2b6fb73ad6d3905dd3768662f8
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-18 20:00:44 +0300

    Update solution.js

--- JS Core/Fundamentals/03. Functions and logic flow/Lab/solutions/04. Unique characters/solution.js
+++ JS Core/Fundamentals/03. Functions and logic flow/Lab/solutions/04. Unique characters/solution.js
@@ -1,18 +1,20 @@
 function solve() {
   let string = Array.from(document.getElementById('string').value)
-  let uniqueChars = ''
-  extractUniqueChars()
+  let uniqueChars = ''  
+  extractUniqueChars(string)
 
-  function extractUniqueChars() {
+  function extractUniqueChars(string) {
     for (let i = 0; i < string.length; i++) {
       if (!uniqueChars.includes(string[i])) {
         uniqueChars += string[i]
+
+      } else {
+        if (string[i].match(/\s/)) {
+          uniqueChars += string[i].match(/\s/)
+        }
       }
     }
 
-
-
+    document.getElementById('result').innerHTML = uniqueChars
   }
-
-  console.log(uniqueChars)
 }
