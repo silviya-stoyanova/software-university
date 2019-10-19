b8517a017bad148c99e98569fb2842c071e2e17b
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-19 12:30:19 +0300

    Add files via upload

---
+++ JS Core/Advanced/07. Exam solutions/04. JS Advanced Retake - 18 April 2019/solution/02. Auto Service/tests.js
@@ -0,0 +1,40 @@
+// constructor()
+// if the the AutoService is instantiated with prop. garageCapacity
+// workInProgress = []
+// backlogWork = []
+
+// get availableSpace()
+
+// repairCar()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let mocha = require('mocha')
+let assert = require('chai').assert
+let AutoService = require('./02. Auto Service_Ресурси')
+
+describe('tests for class AutoService', () => {
+    let service
+    beforeEach(() => {
+        service = new AutoService()
+    })
+
+
+
+
+
+})
