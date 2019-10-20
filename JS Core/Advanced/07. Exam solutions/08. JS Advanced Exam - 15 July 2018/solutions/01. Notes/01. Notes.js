829eb63fbbee1554bf1a4b206a99b5f5b8c4d439
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-20 19:25:42 +0300

    Update 01. Notes.js

--- JS Core/Advanced/07. Exam solutions/08. JS Advanced Exam - 15 July 2018/solutions/01. Notes/01. Notes.js
+++ JS Core/Advanced/07. Exam solutions/08. JS Advanced Exam - 15 July 2018/solutions/01. Notes/01. Notes.js
@@ -10,12 +10,7 @@ function addSticker() {
                 .addClass('button')
                 .click(deleteNote))
             .append($('<h2>').text(title))
-
-            // In HTML5, the <hr> tag defines a thematic break.
-            // In HTML 4.01, the <hr> tag represents a horizontal rule.
-            // However, the <hr> tag may still be displayed as a horizontal rule in visual browsers, but is now defined in semantic terms, rather than presentational terms.
             .append($('<hr>'))
-
             .append($('<p>').text(content))
             .addClass('note-content')
             .appendTo($('#sticker-list'))
