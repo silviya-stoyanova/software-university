b318862805558c6fc8f11160175c0dcfaee488b8
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-25 21:20:49 +0300

    Add files via upload

---
+++ JS Core/Fundamentals/05. Strings and RegEx/Lab/solutions/01. Pascal or Camel Case/solution.js
@@ -0,0 +1,19 @@
+function solve() {
+  // TODO
+  let text = document.getElementById('str1').value
+  let givenCase = document.getElementById('str2').value
+
+  text = text.split(/\s+/)
+    .map(word => word.toLowerCase())
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join('')
+
+  if (givenCase === 'Camel Case') {
+    text = text[0].toLowerCase() + text.slice(1)
+
+  } else if (givenCase !== 'Pascal Case') {
+    text = ['Error!']
+  }
+
+  document.getElementById('result').textContent = text
+}
