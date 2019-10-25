b4b01feb792064717cfbe5f57a97da55a8905cda
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-25 21:10:14 +0300

    Add files via upload

---
+++ JS Core/Fundamentals/04. Arrays and Matrices/Lab/solutions/04. Find Element/solution.js
@@ -0,0 +1,15 @@
+function solve() {
+  // TODO
+  let number = document.getElementById('num').value
+  let inputString = document.getElementById('arr').value
+  let inputArray = JSON.parse(inputString)
+  let index = -1
+  let output = []
+
+  inputArray = inputArray.map((el) => {
+    (el.includes(number) && typeof el === 'string') ? index = el.indexOf(number) : index = -1
+    output.push((index !== -1 ? `true` : `false`) + ` -> ${index}`)
+  })
+
+  document.getElementById('result').textContent = output.join(' ')
+}
