be90b5fb063e69e002a701b5ea9b28c006d01215
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2019-10-25 21:05:28 +0300

    Add files via upload

---
+++ JS Core/Fundamentals/04. Arrays and Matrices/Lab/solutions/01. Sum-First-Last/solution.js
@@ -0,0 +1,12 @@
+function solve() {
+  // TODO
+  let inputArray = document.getElementById('arr').value
+  let inputNums = JSON.parse(inputArray)
+
+  inputNums = inputNums.map(x => x *= inputNums.length)
+    .forEach((num, index) => {
+      let newParagraph = document.createElement('p')
+      newParagraph.textContent = `${index} -> ${num}`
+      document.getElementById('result').appendChild(newParagraph)
+    })
+}
