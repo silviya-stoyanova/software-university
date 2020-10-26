59f4b226f97a165fb8a04aee6057bc801d76bad7
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2020-10-26 21:41:18 +0200

    Added solutions to 2 tasks

---
+++ Programming Basics with C#/01. First steps in coding/01. Greeting by Name/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Greeting_by_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string greeting = $"Hello, {name}!";
+            Console.WriteLine(greeting);
+        }
+    }
+}
