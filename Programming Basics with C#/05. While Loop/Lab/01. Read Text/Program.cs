126b7024b8d332683fd00a1cbcb3ff78821e7c98
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2020-10-31 21:00:21 +0200

    Added solutions to While Loop

---
+++ Programming Basics with C#/05. While Loop/Lab/01. Read Text/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01._Read_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            while (num < 1 || num > 100)
+            {
+                Console.WriteLine("Invalid number!");
+                num = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"The number is: {num}");
+        }
+    }
+}
---
+++ Programming Basics with C#/05. While Loop/Lab/Solutions/02. Password/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02._Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string password = Console.ReadLine();
+            string enteredPassword = Console.ReadLine();
+
+            while (enteredPassword != password)
+            {
+                enteredPassword = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Welcome {name}!");
+        }
+    }
+}
