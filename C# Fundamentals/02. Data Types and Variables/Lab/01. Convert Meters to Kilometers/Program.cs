ebdb04a56b12dee9c15475cd1ab5cd65ce681935
Silviya Stoyanova <55942207+silviya-stoyanova@users.noreply.github.com>
2020-11-02 08:19:13 +0200

    Added solutions to Data Types and Variables

---
+++ C# Fundamentals/02. Data Types and Variables/Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+            double km = meters / 1000.0;
+            Console.WriteLine($"{km:F3}");
+        }
+    }
+}
---
+++ C# Fundamentals/02. Data Types and Variables/Lab/Solutions/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace _03._Exact_Sum_of_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
